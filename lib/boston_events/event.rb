1c8b5d785d9792c022b24ec2f67c15a4a3479252
Liz Burton <liz.burton147@gmail.com>
2017-12-21 11:01:11 +0000

    airport wireless commit

--- lib/boston_events/event.rb
+++ lib/boston_events/event.rb
@@ -20,7 +20,7 @@ class BostonEvents::Event
   def self.scrape_stage_events
     stage_events = []
     doc = Nokogiri::HTML(open("http://calendar.artsboston.org/categories/stage/"))
-    binding.pry
+    # binding.pry
     ## Featured Item - wrapped in article.category-detail
     name = doc.search("article.category-detail h1.p-ttl").text
     dates = doc.search("article.category-detail div.month").text.split("\n")[1].strip + " " + doc.search("article.category-detail div.month").text.split("\n")[2].strip
@@ -28,8 +28,9 @@ class BostonEvents::Event
     ## Listed items - list wrapped in section.list-category; individual events wrapped in article.category-item
     name_list = doc.search("h2.category-ttl")
     index = 0
+    event_name = []
     name_list.each do | name |
-      event_name[index] = name.text
+      event_name[index] = name.text.strip
       index += 1
     end
     binding.pry
