744ae7c7beaa10c7b1365fc53db44263cfa320a9
Liz Burton <liz.burton147@gmail.com>
2017-12-21 01:54:33 +0000

    continued working on scraping stage events

--- lib/boston_events/event.rb
+++ lib/boston_events/event.rb
@@ -21,9 +21,17 @@ class BostonEvents::Event
     stage_events = []
     doc = Nokogiri::HTML(open("http://calendar.artsboston.org/categories/stage/"))
     binding.pry
-    name = doc.search("article.category-detail h1.p-ttl").text  ##Featured event wrapped in article.category-detail
+    ## Featured Item - wrapped in article.category-detail
+    name = doc.search("article.category-detail h1.p-ttl").text
     dates = doc.search("article.category-detail div.month").text.split("\n")[1].strip + " " + doc.search("article.category-detail div.month").text.split("\n")[2].strip
     presented_by = doc.search("article.category-detail p.meta.auth a")[0].text
+    ## Listed items - list wrapped in section.list-category; individual events wrapped in article.category-item
+    name_list = doc.search("h2.category-ttl")
+    # name_list.each_with_index { | event_title, index | name = event_title[index] }
+    dates_list = doc.search("")
+
+    presented_by_list = doc.search("p.meta")
+    # presented_by_list.each_with_index { | presented_by_value, index | presented_by = presented_by_value[index] }
   end
 
   def self.scrape_music_events
