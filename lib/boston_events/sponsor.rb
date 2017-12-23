6d6a7b6cdaafce9040e22097e05a5e1860401517
Liz Burton <liz.burton147@gmail.com>
2017-12-23 20:00:36 +0000

    added code for Sponsor class

--- lib/boston_events/sponsor.rb
+++ lib/boston_events/sponsor.rb
@@ -1,3 +1,23 @@
 class BostonEvents::Sponsor
 
+  attr_accessor :name, :events
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.create_by_name(name)
+    sponsor = self.new
+    sponsor.name = name
+    sponsor.events = []
+    @@all << sponsor
+    sponsor
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.detect { | sponsor | sponsor.name == name } || create_by_name(name)
+  end
+
 end
