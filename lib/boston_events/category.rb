4b3d6db933923b8b942bbd6a0b3861874c9d928b
Liz Burton <liz.burton147@gmail.com>
2017-12-23 11:38:28 +0000

    started coding category class

--- lib/boston_events/category.rb
+++ lib/boston_events/category.rb
@@ -1,5 +1,21 @@
 class BostonEvents::Category
+  attr_accessor :name, :events
 
+  @@all = []
 
+  def self.all
+    @@all
+  end
+
+  def self.create_by_name(name)
+    category = self.new
+    category.name = name
+    category.events = []
+    @@all << category
+  end
+
+  def find_or_create_by_name(name)
+    @@all.detect { | category | category.name == name } | create_by_name(name)
+  end
 
 end
--- lib/boston_events/event.rb
+++ lib/boston_events/event.rb
@@ -1,10 +1,10 @@
 class BostonEvents::Event
-  attr_accessor :name, :dates, :presented_by, :venue
-  @@all = []
-
-  def self.all
-    @@all
-  end
+  attr_accessor :name, :dates, :presented_by, :venue, :category
+  # @@all = []
+  #
+  # def self.all
+  #   @@all
+  # end
 
   def self.list_events(category)
     if category == 'top-ten'
@@ -16,8 +16,8 @@ class BostonEvents::Event
 
   def self.scrape_events(category)
     doc = Nokogiri::HTML(open("http://calendar.artsboston.org/categories/#{category}/"))
-    scrape_featured_item(doc)
-    scrape_listed_items(doc)
+    scrape_featured_item(doc, category)
+    scrape_listed_items(doc, category)
   end
 
   def self.scrape_top_ten
@@ -28,27 +28,30 @@ class BostonEvents::Event
       dates = this_event.search("div.left-event-time.evt-date-bubble")
       event.dates = get_event_dates(dates)
       event.presented_by = this_event.search("p.meta")[0].text.strip.gsub("Presented by ","").gsub(/  at .*/,"")
-      @@all << event
+      event.category = BostonEvents::Category.find_or_create_by_name("top-ten")
+      # @@all << event
     end #each with index
   end
 
-  def self.scrape_featured_item(doc)
+  def self.scrape_featured_item(doc, category_name)
     event = self.new
     event.name = doc.search("article.category-detail h1.p-ttl").text
     dates = doc.search("article.category-detail div.month") ## Does this work for all featured items????
     event.dates = get_event_dates(dates)
     event.presented_by = doc.search("article.category-detail p.meta.auth a")[0].text
-    @@all << event
+    event.category = BostonEvents::Category.find_or_create_by_name(category_name)
+    # @@all << event
   end
 
-  def self.scrape_listed_items(doc)
+  def self.scrape_listed_items(doc, category_name)
     item_list = doc.search("section.list-category article.category-itm").each_with_index do | this_event, index |
       event = self.new
       event.name = this_event.search("h2.category-ttl").text.strip
       dates = this_event.search("div.left-event-time.evt-date-bubble")
       event.dates = get_event_dates(dates)
       event.presented_by = this_event.search("p.meta").text.strip.gsub("Presented by ","").gsub(/  at .*/,"")
-      @@all << event
+      event.category = BostonEvents::Category.find_or_create_by_name(category_name)
+      # @@all << event
     end #each with index
   end
 
