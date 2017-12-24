0e947c104519cf5810a3b0983c8f04e1a7a077f3
Liz Burton <liz.burton147@gmail.com>
2017-12-24 13:32:05 +0000

    refactoring CLI

--- lib/boston_events/cli.rb
+++ lib/boston_events/cli.rb
@@ -1,19 +1,20 @@
 # CLI Controller
 class BostonEvents::CLI
+  attr_accessor :category
 
   def call
     puts "What's going on in Boston"
-    select_category
-    BostonEvents::Event.list_events(category)
-    puts_events(category)
-
+    input = nil
+    while input != "exit"
+      category = select_category
+      BostonEvents::Event.list_events(category)
+      list_events_in_category(category)
+    end #while
   end
 
   def select_category
     puts "Select a category by number or type exit"
     list_categories
-    input = nil
-    while input != "exit"
       input = gets.strip.downcase
       case input
       when "1"
@@ -32,12 +33,11 @@ class BostonEvents::CLI
         abort ("Thanks for stopping by -- come back often to check out what's going on around town!")
       else
         puts "I'm not sure what you want - please enter a category number or type exit"
-        menu
+        select_category
       end # Case statement
-      category = BostonEvents::Category.find_or_create_by_name(category_name)
-
-    end # While
-  end # #menu
+      BostonEvents::Category.find_or_create_by_name(category_name)
+    # end # While
+  end # #select_category
 
   def list_categories
     puts "1. Stage"
@@ -49,19 +49,40 @@ class BostonEvents::CLI
     puts "7. All events (not yet implemented)"
   end # #list_categories
 
-  def puts_events(category)
+  def list_events_in_category(category)
     puts
     puts "Events in category: #{category.name.capitalize}"
     category.events.each.with_index(1) do | event, index |
       puts "#{index}. #{event.name}, #{event.dates}, presented by #{event.sponsor.name}"
     end #each
     puts "Select an event to see more information or type 'list' to return to the category list."
+    choose_event_to_view(category)
+  end # #list_events_in_category
+
+  def choose_event_to_view(category)
     input = nil
     while input != 'exit'
       input = gets.strip.downcase
-      choose_option(input, category)
+      if input.to_i >= 1 && input.to_i <= category.events.length
+        return_event_info(category, input)
+      elsif input == "list"
+        call
+      elsif input == "exit"
+        abort ("Thanks for stopping by -- come back often to check out what's going on around town!")
+      else
+        puts "I'm not sure what you want - please enter a valid option."
+      end # if/elsif/else
     end
-  end # #puts_events
+  end
+
+  def return_event_info(category, input)
+    category.events.detect.with_index(1) do | event, index |
+      if input.to_i == index
+        puts_event_info(event)
+      end
+    end # detect
+    puts "Select another event or type 'list' to return to the category list."
+  end
 
   def puts_event_info(event)
     puts "Event: #{event.name}"
@@ -70,27 +91,6 @@ class BostonEvents::CLI
     puts "Venue: #{event.venue.name}"
     puts "Check for deals: #{event.deal_url}" if event.deal_url
     puts "Official website: #{event.website_url}" if event.website_url
-  end # #puts_event_info
-
-  def choose_option(input, category)
-    if input.to_i >= 1 && input.to_i <= category.events.length
-      select_event(category, input)
-    elsif input == "list"
-      menu
-    elsif input == "exit"
-      abort ("Thanks for stopping by -- come back often to check out what's going on around town!")
-    else
-      puts "I'm not sure what you want - please enter a valid option."
-    end # if/elsif/else
-  end
-
-  def select_event(category, input)
-    category.events.detect.with_index(1) do | event, index |
-      if input.to_i == index
-        puts_event_info(event)
-      end
-    end # detect
-    puts "Select another event or type 'list' to return to the category list."
   end
 
 end # Class
