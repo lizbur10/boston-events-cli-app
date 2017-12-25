586916a6bc6564a0f75cf2e9041d978bb5bd3df9
Liz Burton <liz.burton147@gmail.com>
2017-12-25 03:36:40 +0000

    refactored list_events method in Event class

--- lib/boston_events/event.rb
+++ lib/boston_events/event.rb
@@ -2,35 +2,6 @@ class BostonEvents::Event
   attr_accessor :name, :dates, :sponsor, :venue, :deal_url, :website_url, :category
   @@all = []
 
-  def self.all
-    @@all
-  end
-
-  def save
-    if !self.class.all.detect { | saved_event | saved_event.name == self.name }
-      @@all << self
-    end
-  end
-
-  def self.list_events(category) ##This needs to be refactored
-    if category.events.length == 0
-      if category.name == 'top-ten'
-        doc = Nokogiri::HTML(open(SCRAPE_SELECTORS['top-ten'][:url]))
-        item_list = doc.search(SCRAPE_SELECTORS['top-ten'][:iterate_over]).each_with_index do | this_event, index |
-          scrape_events('top-ten',category,this_event)
-        end
-      else
-        doc = Nokogiri::HTML(open(SCRAPE_SELECTORS['featured'][:url] + "#{category.name}/"))
-        item_list = doc.search(SCRAPE_SELECTORS['featured'][:iterate_over]).each_with_index do | this_event, index |
-          scrape_events('featured',category,this_event)
-        end
-        item_list = doc.search(SCRAPE_SELECTORS['listed'][:iterate_over]).each_with_index do | this_event, index |
-          scrape_events('listed',category,this_event)
-        end
-      end
-    end
-  end
-
   SCRAPE_SELECTORS = {
     "top-ten" => {
       url: "http://calendar.artsboston.org/",
@@ -58,21 +29,49 @@ class BostonEvents::Event
     }
   }
 
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self if !self.class.all.detect { | saved_event | saved_event.name == self.name }
+  end
+
+  def self.list_events(category)
+    if category.events.length == 0
+      if category.name == 'top-ten'
+        doc = Nokogiri::HTML(open(SCRAPE_SELECTORS[category.name][:url]))
+        scrape_iterator(category, category.name, doc)
+      else
+        doc = Nokogiri::HTML(open(SCRAPE_SELECTORS['featured'][:url] + "#{category.name}/"))
+        scrape_iterator(category, 'featured', doc)
+        scrape_iterator(category, 'listed', doc)
+      end # if/else
+    end # if
+  end
+
+  def self.scrape_iterator(category, type, doc)
+    item_list = doc.search(SCRAPE_SELECTORS[type][:iterate_over]).each_with_index do | this_event, index |
+      scrape_events(type,category,this_event)
+    end
+  end
+
   def self.scrape_events(type, category, this_event)
     event = self.new
     event.name = this_event.search(SCRAPE_SELECTORS[type][:name]).text.strip
+
     dates = this_event.search(SCRAPE_SELECTORS[type][:dates])
     event.dates = get_event_dates(dates)
+
     sponsor_name = this_event.search(SCRAPE_SELECTORS[type][:sponsor_and_venue_names])[0].text.strip.gsub("Presented by ","").gsub(/  at .*/,"")
     event.add_sponsor(sponsor_name)
+
     venue_name = this_event.search(SCRAPE_SELECTORS[type][:sponsor_and_venue_names])[0].text.split(" at ")[1].strip
     event.add_venue(venue_name)
-    if this_event.search(SCRAPE_SELECTORS[type][:event_urls])[1]
-      event.deal_url = this_event.search(SCRAPE_SELECTORS[type][:event_urls])[1].attribute("href")
-    end
-    if this_event.search(SCRAPE_SELECTORS[type][:event_urls])[0]
-      event.website_url = this_event.search(SCRAPE_SELECTORS[type][:event_urls])[0].attribute("href")
-    end
+
+    event.deal_url = this_event.search(SCRAPE_SELECTORS[type][:event_urls])[1].attribute("href") if this_event.search(SCRAPE_SELECTORS[type][:event_urls])[1]
+    event.website_url = this_event.search(SCRAPE_SELECTORS[type][:event_urls])[0].attribute("href") if this_event.search(SCRAPE_SELECTORS[type][:event_urls])[0]
+
     event.add_category(category)
     event.save
   end
